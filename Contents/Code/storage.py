e81bf440117f5fd052d22e6335491b3a5bdea01d
Alexander Shvets <alexander.shvets@gmail.com>
2016-04-16 13:43:56 -0400

    generalize code

--- Contents/Code/storage.py
+++ Contents/Code/storage.py
@@ -1,26 +1,28 @@
 import json
+import copy
 
-class Storage():
+class Storage:
     def __init__(self, storage, file_name):
         self.storage = storage
         self.file_name = file_name
 
-    def reset(self):
+        self.clear()
+
+    def clear(self):
         self.data = []
 
     def storage_exist(self):
         return self.storage.file_exists(self.file_name)
 
     def load(self):
-        self.reset()
+        self.clear()
 
         if self.storage_exist():
             self.data = json.loads(self.storage.load(self.file_name))
 
-    def save(self, config=None):
-        if config:
-            for item in config.items():
-                self.data.append(item)
+    def save(self, data=None):
+        if data:
+            self.data = copy.copy(data)
 
         self.storage.save(self.file_name, json.dumps(self.data, indent=4))
 
